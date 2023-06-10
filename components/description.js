fa955d86967bd2799980e2480340ad62638f863d
nonamexx123 <135545348+nonamexx123@users.noreply.github.com>
2023-06-10 17:04:26 +0530

    Basic template
    Basic template

---
+++ components/description.js
@@ -0,0 +1,8 @@
+export default function Description() {
+    return (
+         <div>
+            <h1>Kadanes Algorithm</h1>
+            <p>Description......</p>
+         </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+import Description from '../components/description';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Description />
+    </div>
+  )
+}
